8554bac7fa252007c0cf09706d44fcb4600faf66
VICTOR VICENTE VELEPUCHA BONETT <victor.velepucha@epn.edu.ec>
2020-06-16 22:21:09 -0500

    Initial Version

---
+++ app/src/main/java/com/epnfis/holamundo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.epnfis.holamundo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonEnviarSaludo.setOnClickListener {
+            Toast.makeText(this,"Hola mundo otra vez!!",Toast.LENGTH_LONG).show()
+        }
+    }
+}
